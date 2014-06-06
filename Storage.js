83226f6633c4cf369cb74463f9b1b5fa83cf96cb
Klederson Bueno <klederson@klederson.com>
2014-06-06 20:04:15 -0300

    Fix first level bug!

--- Storage.js
+++ Storage.js
@@ -23,91 +23,94 @@
  */
 //Add Storage support for objects
 Storage.prototype.__walker = function(path,o) {
-  //Validate if path is an object otherwise returns false
-  if(typeof path !== "object")
-    return false;
+    //Validate if path is an object otherwise returns false
+    if(typeof path !== "object")
+        return undefined;
 
-  if(path.length === 0){
-    return o;
-  }
-
-  for(var i in path){
-    var prop = path[i];
-    //Check if path step exists
-    if(o.hasOwnProperty(prop)){
-      var val = o[prop];
-      if(typeof val == 'object'){
-        path.splice(0,1);
-        return this.__walker(path,val);
-      } else {
-        return val;
-      }
+    if(path.length === 0){
+        return o;
     }
-  }
+
+    for(var i in path){
+        var prop = path[i];
+        //Check if path step exists
+        if(o.hasOwnProperty(prop)){
+            var val = o[prop];
+            if(typeof val == 'object'){
+                path.splice(0,1);
+                return this.__walker(path,val);
+            } else {
+                return val;
+            }
+        }
+    }
 };
 
 Storage.prototype.setObj = function(key, value) {
 
-  var path = key.split('.');
+    var path = key.split('.');
 
-  //First level is always the localStorage key pair item
-  var _key = path[0];
-  var os = this.getItem(_key) ? JSON.parse(this.getItem(_key)) : null; //general storage key pair element
-  path.splice(0,1);
+    //First level is always the localStorage key pair item
+    var _key = path[0];
+    var os = this.getItem(_key) !== null ? JSON.parse(this.getItem(_key)) : null; //general storage key pair element
+    path.splice(0,1);
 
-  if(os === null) {
-    os = {};
-    this.setItem(_key,JSON.stringify(os));
-  }
-
-  var innerWalker = function(path,o) {
-
-    //Validate if path is an object otherwise returns false
-    if(typeof path !== "object")
-      return false;
-
-    if(path.length == 1) {
-      o[path[0]] = value;
-      return o;
+    if(os === null) {
+        os = {};
+        this.setItem(_key,JSON.stringify(os));
     }
 
-    var val = null;
+    var innerWalker = function(path,o) {
 
-    for(var i in path){
-      var prop = path[i];
-      //Check if path step exists
-      if(o.hasOwnProperty(prop)) {
-        val = o[prop];
-        if(typeof val == 'object'){
-          path.splice(0,1);
-          return innerWalker(path,val);
+        //Validate if path is an object otherwise returns false
+        if(typeof path !== "object")
+            return undefined;
+
+        if(path.length == 1) {
+            o[path[0]] = value;
+            return o;
+        } else if(path.length === 0) {
+            os = value;
+            return os;
         }
-      } else {
-        //create depth
-        o[prop] = {};
-        val = o[prop];
-        path.splice(0,1);
-        return innerWalker(path,val);
-      }
-    }
-  };
+        
+        var val = null;
 
-  innerWalker(path,os);
+        for(var i in path){
+            var prop = path[i];
+            //Check if path step exists
+            if(o.hasOwnProperty(prop)) {
+                val = o[prop];
+                if(typeof val == 'object'){
+                    path.splice(0,1);
+                    return innerWalker(path,val);
+                }
+            } else {
+                //create depth
+                o[prop] = {};
+                val = o[prop];
+                path.splice(0,1);
+                return innerWalker(path,val);
+            }
+        }
+    };
 
-  this.setItem(_key,JSON.stringify(os));
+    innerWalker(path,os);
+    
+    this.setItem(_key,JSON.stringify(os));
 };
 
 Storage.prototype.getObj = function(key) {
-  key = key.split('.');
+    key = key.split('.');
 
-  //First level is always the localStorage key pair item
-  var _key = key[0];
-  var o = this.getItem(_key) ? JSON.parse(this.getItem(_key)) : null;
+    //First level is always the localStorage key pair item
+    var _key = key[0];
+    var o = this.getItem(_key) ? JSON.parse(this.getItem(_key)) : null;
 
-  if(o === null)
-    return false;
+    if(o === null)
+        return undefined;
 
-  key.splice(0,1);
+    key.splice(0,1);
 
-  return this.__walker(key,o);
+    return this.__walker(key,o);
 };
